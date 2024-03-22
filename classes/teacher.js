3fdadeadd5db2a2b51e2f8ef02473df8ee4e866a
Smanga Ngcamu <ngcamukhetha@gamil.com>
2024-03-22 17:18:38 +0200

    inheritnce project completed

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,26 @@
 const Person = require('./person');
 
 // Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience){
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    if (!Array.isArray(teachers)) {
+      throw new Error("combinedYearsOfExperience only takes an array as an argument.");
+    }
+
+    return teachers.reduce((totalExperience, teacher) => {
+      if (!(teacher instanceof Teacher)) {
+        throw new Error("All items in array must be Teacher class instances.");
+      }
+      return totalExperience + teacher.yearsOfExperience;
+    }, 0);
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
